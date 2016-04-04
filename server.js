157f4d4fad0810d19fe1354adfac56a939a3f5a0
Nick Frazier <nrf@nickfrazier.com>
2016-04-03 20:08:42 -0400

    New project setup

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+// var Stamper = require('./stamper');
+
+var app = express();
+// var stamper = new Stamper();
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', function(req, res, next) {
+//   next();
+// });
+
+app.get('/:id', function (req, res) {
+  var id = req.params.id;
+  res.send(id);
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+  console.log('Node.js listening on port ' + port + '...');
+});
